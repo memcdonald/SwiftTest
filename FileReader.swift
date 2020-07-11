ac14af8eeac882ff6c2b2cc1bb3edbe2eb5eb7f4
Chad McDonald <me@chadmcdonald.net>
2020-07-11 09:08:57 -0400

    Initial Commit

---
+++ FileReader.swift
@@ -0,0 +1,9 @@
+import Foundation
+class FileReader {
+    static func read(filename: String) -> String? {
+        let fileManager = FileManager.default
+        let currentDirectoryURL = URL(fileURLWithPath: fileManager.currentDirectoryPath)
+        let fileURL = currentDirectoryURL.appendingPathComponent(filename)
+        return try? String(contentsOf: fileURL, encoding: .utf8)
+    }
+}
